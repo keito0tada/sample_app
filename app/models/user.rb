6b4460feff4d6e0c0e91a31e498dc73e6c0399b3
keito0tada <keito0tada@gmail.com>
2024-02-01 16:34:48 +0900

    add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    validates :name, presence: true, length: { maximum: 50 }
+    validates :email, presence: true, length: { maximum: 255 }
+    validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+    validates :email, uniqueness: { case_sensitive: false}
 end
