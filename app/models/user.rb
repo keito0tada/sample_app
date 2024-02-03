f9484fe43dcb31d11989307e9e72324c2177f9ad
keito0tada <keito0tada@gmail.com>
2024-02-03 14:26:15 +0900

    downcase emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
+    before_save { self.email = self.email.downcase}
     validates :name, presence: true, length: { maximum: 50 }
     validates :email, presence: true, length: { maximum: 255 }
     validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-    validates :email, uniqueness: { case_sensitive: false}
+    validates :email, uniqueness: true
 end
