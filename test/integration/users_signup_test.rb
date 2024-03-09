1a22246bca4248bb3a13fca0ec1d59ed241f9bf9
keito0tada <keito0tada@gmail.com>
2024-03-10 00:16:46 +0900

    add valid information test

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -13,4 +13,14 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/new'
     assert_select 'div#error_explanation'
   end
+    test "valid signup information" do
+      assert_difference 'User.count', 1 do
+        post users_path, params: { user: { name: "example user",
+                                           email: "user@example.com",
+                                           password: "password",
+                                           password_confirmation: "password" }}
+      end
+      follow_redirect!
+      assert_template 'users/show'
+    end
 end
