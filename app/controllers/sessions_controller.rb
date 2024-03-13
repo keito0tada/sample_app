204b978d23cf540275e76be607fea9cf73679aba
keito0tada <keito0tada@gmail.com>
2024-03-13 13:10:12 +0900

    add current_user()

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,11 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase())
     if user && user.authenticate(params[:session][:password])
+      reset_session
+      log_in user
+      redirect_to user
     else
-      flash[:danger] = "Invalid email/password combination"
+      flash.now[:danger] = "Invalid email/password combination"
       render 'new', status: :unprocessable_entity
     end
   end
