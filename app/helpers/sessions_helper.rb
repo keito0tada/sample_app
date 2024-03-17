3fb2d6eaa2648c1819e15ffcdf7017b3faa8a193
keito0tada <keito0tada@gmail.com>
2024-03-17 15:58:38 +0900

    forget user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,7 +5,7 @@ module SessionsHelper
 
     def remember(user)
         user.remember
-        cookies.permanent.encrypted[:user_id] = user_id
+        cookies.permanent.encrypted[:user_id] = user.id
         cookies.permanent[:remember_token] = user.remember_token
     end
 
@@ -24,8 +24,15 @@ module SessionsHelper
     def logged_in?
         !current_user.nil?
     end
+
+    def forget(user)
+        user.forget
+        cookies.delete(:user_id)
+        cookies.delete(:remember_token)
+    end
     
     def log_out
+        forget(current_user)
         reset_session
         @current_user = nil
     end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -17,6 +17,10 @@ module ActiveSupport
     def is_logged_in?
       !session[:user_id].nil?
     end
+
+    def log_in_as(user)
+      session[:user_id] = user.id
+    end
   end
 
 end
